f9ee5b194006dd112e59f7a8e2bb5c27569243c8
natnael772 <natnaeldeyas0@gmail.com>
2024-03-19 12:37:57 +0300

    feat: Add choose option display markup buttons

---
+++ src/service1/service1-formatter.ts
@@ -0,0 +1,26 @@
+import { Markup } from 'telegraf';
+import { InlineKeyboardButtons } from '../components/button';
+
+class Service1Formatter {
+  constructor() {}
+  chooseOptionDisplay() {
+    return [
+      Markup.keyboard([
+        [Markup.button.callback('Option_1', 'Option 1'), Markup.button.callback('Option_2', 'Option 2')],
+        [Markup.button.callback('Option_3', 'Option 3'), Markup.button.callback('Option_4', 'Option 4')],
+        [Markup.button.callback('Option_5', 'Option 5'), Markup.button.callback('Option_6', 'Option 6')],
+        [Markup.button.callback('Back', 'Back'), Markup.button.callback('Next', 'Next')],
+      ]).resize(),
+    ];
+  }
+}
+
+export default Service1Formatter;
+
+// export const formatJoinMessage = (first_name: string) => {
+//   return `Hey ${first_name} 👋
+
+// It seems like you haven't joined our channel yet,
+// the channel is where we post questions asked by you and others,
+// Join using the button below!`;
+// };
