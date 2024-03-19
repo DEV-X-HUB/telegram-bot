de0a8ff0948cd80965bbcde9e1bd13f85767c130
=Natty-7r <nati7fekadu@gmail.com>
2024-03-19 10:26:22 +0300

    feat: add regstration service

--- src/registration/restgration.service.ts
+++ src/registration/restgration.service.ts
@@ -1,3 +1,38 @@
+import User from '../model/user';
+
 class RegistrationService {
   constructor() {}
+
+  async isUserRegistered(phoneNumber: string): Promise<boolean> {
+    try {
+      const user = await User.findOne({ phone_number: phoneNumber });
+      if (user) return true;
+      return false;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async registerUser(phoneNumber: string, userData: any) {
+    try {
+      const doesUserExist = await this.isUserRegistered(phoneNumber);
+      if (doesUserExist) return { success: false, message: 'user exits', data: null };
+      return await this.createUser(userData);
+    } catch (error) {}
+    return { success: false, message: 'unkown error', data: null };
+  }
+
+  async createUser(userData: any) {
+    try {
+      const newUser = new User({
+        ...userData,
+      });
+      return { success: true, data: await newUser.save(), message: 'user created' };
+    } catch (error: any) {
+      return { success: false, data: null, message: error?.message };
+    }
+  }
 }
+
+export default RegistrationService;
