9625fe1543f8201f8266d648de5b5a6d0d4f456e
natnael772 <natnaeldeyas0@gmail.com>
2024-03-19 12:39:40 +0300

    feat: Add choose option handler

--- src/service1/service1.controller.ts
+++ src/service1/service1.controller.ts
@@ -8,6 +8,30 @@ class Service1Controller {
     ctx.reply('Choose an option', ...service1Formatter.chooseOptionDisplay());
     return ctx.wizard.next();
   }
+
+  async chooseOptionHandler(ctx: any) {
+    const option = ctx.message.text;
+    // console.log(`the option is ${option}`);
+
+    if (option === 'Back') {
+      // go back to the previous scene
+      return ctx.scene.enter('start');
+    }
+
+    if (option == 'Next') {
+      ctx.reply('Next');
+      ctx.reply('Next Option. Choose an option', ...service1Formatter.chooseNextOptionDisplay());
+      return ctx.wizard.next();
+    }
+
+    console.log('exists ', ctx.scene.scenes.has(option));
+
+    if (ctx.scene.scenes.has(option)) {
+      return ctx.scene.enter(option);
+    } else {
+      return ctx.reply('Unknown option. Please choose a valid option.');
+    }
+  }
 }
 
 export default Service1Controller;
