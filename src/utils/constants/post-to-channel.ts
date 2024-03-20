a0dde6263ba45027b9d9224ad5e2752ae2e11dbc
natnael772 <natnaeldeyas0@gmail.com>
2024-03-20 10:28:01 +0300

    chore: minor update

--- src/utils/constants/post-to-channel.ts
+++ src/utils/constants/post-to-channel.ts
@@ -1,4 +1,4 @@
 // Function that posts a message to a channel
 export const postToChannel = async (ctx: any, message: string) => {
-  await ctx.telegram.sendMessage(process.env.CHANNEL_ID, message);
+  return await ctx.telegram.sendMessage(process.env.CHANNEL_ID, message);
 };
