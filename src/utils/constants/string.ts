f9ed771b8054423d0f828983352528815a4f80f9
=Natty-7r <nati7fekadu@gmail.com>
2024-03-21 09:40:19 +0300

    feat: string uitility function

---
+++ src/utils/constants/string.ts
@@ -0,0 +1,25 @@
+import { TableInlineKeyboardButtons, TableMarkupKeyboardButtons } from '../../components/button';
+
+export const areEqaul = (text1: string, text2: string, ignoreCase?: boolean) => {
+  if (ignoreCase) return text1.toLocaleLowerCase().trim() == text2.toLocaleLowerCase().trim();
+  return text1.trim() == text2.trim();
+};
+
+export const isInInlineOption = (text: string, options: TableInlineKeyboardButtons | TableMarkupKeyboardButtons) => {
+  let exists = false;
+  options.forEach((rowOption) => {
+    rowOption.forEach((option) => {
+      if (areEqaul(option.cbString, text, true)) exists = true;
+    });
+  });
+  return exists;
+};
+export const isInMarkUPOption = (text: string, options: TableInlineKeyboardButtons | TableMarkupKeyboardButtons) => {
+  let exists = false;
+  options.forEach((rowOption) => {
+    rowOption.forEach((option) => {
+      if (areEqaul(option.text, text, true)) exists = true;
+    });
+  });
+  return exists;
+};
