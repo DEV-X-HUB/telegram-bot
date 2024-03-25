c47058727f48b5c35dec71c0c281afa7a14de3b8
natnael772 <natnaeldeyas0@gmail.com>
2024-03-25 14:57:30 +0300

    feat: delete markup keyboard

--- src/utils/constants/chat.ts
+++ src/utils/constants/chat.ts
@@ -8,3 +8,18 @@ export const deleteMessageWithCallback = async (ctx: any) => {
     chat_id: ctx.callbackQuery.message.chat.id,
   });
 };
+
+export const deleteKeyboardMarkup = async (ctx: any) => {
+  // it should be sent before any message
+  const messageOptions = {
+    reply_markup: {
+      remove_keyboard: true,
+    },
+  };
+
+  await ctx.reply('.', messageOptions);
+  await deleteMessage(ctx, {
+    message_id: (parseInt(ctx.message.message_id) + 1).toString(),
+    chat_id: ctx.message.chat.id,
+  });
+};
