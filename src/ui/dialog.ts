aba69061785b1f79048d6e7aa6c3d6b911397270
natnael772 <natnaeldeyas0@gmail.com>
2024-03-28 01:28:00 +0300

    feat: Add notification dialog

---
+++ src/ui/dialog.ts
@@ -0,0 +1,11 @@
+import Bot from '../loaders/bot';
+
+// Display notification(popup) when user click on inline keyboard
+async function displayDialog(displayMessage: string, callbackQuery: string, ctx: any) {
+  const bot = Bot();
+  if (bot != null) {
+    await ctx.answerCbQuery(displayMessage, { show_alert: true });
+  }
+}
+
+export { displayDialog };
