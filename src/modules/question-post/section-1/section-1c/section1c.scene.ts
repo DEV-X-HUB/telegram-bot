c91e8fc380556ad9c02adac07854a31e811f8318
natnael772 <natnaeldeyas0@gmail.com>
2024-04-01 14:23:41 +0300

    chore: move section1c folder to section1

---
+++ src/modules/question-post/section-1/section-1c/section1c.scene.ts
@@ -0,0 +1,22 @@
+import { Scenes } from 'telegraf';
+import QuestionPostSection1cController from './section1c.controller';
+
+const section1cController = new QuestionPostSection1cController();
+const QuestionPostSectionCScene = new Scenes.WizardScene(
+  'Section 1C',
+  section1cController.start,
+  section1cController.choosePaperTimeStamp,
+  section1cController.arBrOption,
+  section1cController.choooseWoreda,
+  section1cController.chooseServiceType1,
+  section1cController.chooseServiceType2,
+  section1cController.chooseServiceType3,
+  section1cController.yearOfConfirmation,
+  section1cController.IDFirstOption,
+  section1cController.enterLastDigit,
+  section1cController.enterDescription,
+  section1cController.attachPhoto,
+  section1cController.editPost,
+);
+
+export default QuestionPostSectionCScene;
