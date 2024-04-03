0e8e89c4bb6dd75772689320c32fa951a9d76811
natnael772 <natnaeldeyas0@gmail.com>
2024-04-03 19:24:36 +0300

    feat: Edit data

--- src/modules/question-post/section-1/section-1c/section1c.controller.ts
+++ src/modules/question-post/section-1/section-1c/section1c.controller.ts
@@ -2,9 +2,9 @@ import config from '../../../../config/config';
 import { deleteKeyboardMarkup, deleteMessage, deleteMessageWithCallback } from '../../../../utils/constants/chat';
 import { areEqaul, isInInlineOption, isInMarkUPOption } from '../../../../utils/constants/string';
 
-import Section1CFormatter from './section1c.formatter';
+import QuestionPostSection1CFormatter from './section1c.formatter';
 // import QuestionService from './question.service';
-const section1cFormatter = new Section1CFormatter();
+const section1cFormatter = new QuestionPostSection1CFormatter();
 
 let imagesUploaded: any[] = [];
 const imagesNumber = 4;
@@ -275,7 +275,7 @@ class QuestionPostSectionCController {
           console.log('preview edit');
           ctx.wizard.state.editField = null;
           await deleteMessageWithCallback(ctx);
-          // ctx.reply(...section1cFormatter.editPreview(state), { parse_mode: 'HTML' });
+          ctx.reply(...section1cFormatter.editPreview(state), { parse_mode: 'HTML' });
           return ctx.wizard.next();
         }
 
@@ -315,123 +315,138 @@ class QuestionPostSectionCController {
       // }
     }
   }
+
+  async editData(ctx: any) {
+    const state = ctx.wizard.state;
+    const fileds = [
+      'paper_stamp',
+      'ar_br',
+      'woreda',
+      'service_type_1',
+      'service_type_2',
+      'service_type_3',
+      'year_of_confirmation',
+      'bi_di',
+      'last_digit',
+      // 'location',
+      'description',
+      'photo',
+      'cancel',
+    ];
+    const callbackQuery = ctx?.callbackQuery;
+    const editField = ctx.wizard.state?.editField;
+    if (!callbackQuery) {
+      // changing field value
+      const messageText = ctx.message.text;
+      if (!editField) return await ctx.reply('invalid input ');
+
+      // const validationMessage = questionPostValidator(ctx.wizard.state.editField, ctx.message.text);
+      // if (validationMessage != 'valid') return await ctx.reply(validationMessage);
+
+      ctx.wizard.state[editField] = messageText;
+      await deleteKeyboardMarkup(ctx);
+      // await deleteMessage(ctx, {
+      //   message_id: (parseInt(ctx.message.message_id) - 1).toString(),
+      //   chat_id: ctx.message.chat.id,
+      // });
+      return ctx.reply(...section1cFormatter.editPreview(state), { parse_mode: 'HTML' });
+    }
+
+    // if callback exists
+    // save the mesage id for later deleting
+    ctx.wizard.state.previousMessageData = {
+      message_id: ctx.callbackQuery.message.message_id,
+      chat_id: ctx.callbackQuery.message.chat.id,
+    };
+    const callbackMessage = callbackQuery.data;
+
+    if (callbackMessage == 'post_data') {
+      console.log('Posted Successfully');
+      return ctx.reply(...section1cFormatter.postingSuccessful());
+      // registration
+      // api call for registration
+      // const response = await QuestionService.createQuestionPost(ctx.wizard.state, callbackQuery.from.id);
+
+      // if (response.success) {
+      //   ctx.wizard.state.status = 'pending';
+      //   await deleteMessageWithCallback(ctx);
+      //   await ctx.reply(...postingFormatter.postingSuccessful());
+      //   return ctx.scene.enter('start');
+      // }
+
+      const registrationAttempt = parseInt(ctx.wizard.state.registrationAttempt);
+
+      // ctx.reply(...postingFormatter.postingError());
+      if (registrationAttempt >= 2) {
+        await deleteMessageWithCallback(ctx);
+        return ctx.scene.enter('start');
+      }
+      return (ctx.wizard.state.registrationAttempt = registrationAttempt ? registrationAttempt + 1 : 1);
+    } else if (callbackMessage == 'editing_done') {
+      // await deleteMessageWithCallback(ctx);
+
+      await ctx.reply(...section1cFormatter.preview(state));
+      return ctx.wizard.back();
+    }
+
+    if (fileds.some((filed) => filed == callbackQuery.data)) {
+      // selecting field to change
+      ctx.wizard.state.editField = callbackMessage;
+      await ctx.telegram.deleteMessage(
+        ctx.wizard.state.previousMessageData.chat_id,
+        ctx.wizard.state.previousMessageData.message_id,
+      );
+      await ctx.reply(...((await section1cFormatter.editFieldDispay(callbackMessage)) as any));
+      if (areEqaul(callbackQuery.data, 'photo', true)) return ctx.wizard.next();
+      return;
+    }
+
+    if (editField) {
+      //  if edit filed is selected
+
+      ctx.wizard.state[editField] = callbackMessage;
+      await deleteMessageWithCallback(ctx);
+      ctx.wizard.state.editField = null;
+      return ctx.reply(...section1cFormatter.editPreview(state), { parse_mode: 'HTML' });
+    }
+  }
+  async editPhoto(ctx: any) {
+    const messageText = ctx.message?.text;
+    if (messageText && areEqaul(messageText, 'back', true)) {
+      await deleteMessage(ctx, {
+        message_id: (parseInt(messageText.message_id) - 1).toString(),
+        chat_id: messageText.chat.id,
+      });
+      ctx.reply(...section1cFormatter.editPreview(ctx.wizard.state), { parse_mode: 'HTML' });
+      return ctx.wizard.back();
+    }
+
+    // check if image is attached
+    if (!ctx.message.photo) return ctx.reply(...section1cFormatter.photoPrompt());
+
+    // Add the image to the array
+    imagesUploaded.push(ctx.message.photo[0].file_id);
+
+    // Check if all images received
+    if (imagesUploaded.length === imagesNumber) {
+      const file = await ctx.telegram.getFile(ctx.message.photo[0].file_id);
+
+      const mediaGroup = imagesUploaded.map((image: any) => ({
+        media: image,
+        type: 'photo',
+        caption: image == imagesUploaded[0] ? 'Here are the images you uploaded' : '',
+      }));
+
+      await ctx.telegram.sendMediaGroup(ctx.chat.id, mediaGroup);
+
+      // Save the images to the state
+      ctx.wizard.state.photo = imagesUploaded;
+
+      // empty the images array
+      // imagesUploaded.length = 0;
+      ctx.reply(...section1cFormatter.editPreview(ctx.wizard.state), { parse_mode: 'HTML' });
+      return ctx.wizard.back();
+    }
+  }
 }
-
-// async editData(ctx: any) {
-//   const state = ctx.wizard.state;
-//   const fileds = ['ar_br', 'bi_di', 'woreda', 'last_digit', 'location', 'description', 'photo', 'cancel'];
-//   const callbackQuery = ctx?.callbackQuery;
-//   const editField = ctx.wizard.state?.editField;
-//   if (!callbackQuery) {
-//     // changing field value
-//     const messageText = ctx.message.text;
-//     if (!editField) return await ctx.reply('invalid input ');
-
-//     // const validationMessage = questionPostValidator(ctx.wizard.state.editField, ctx.message.text);
-//     // if (validationMessage != 'valid') return await ctx.reply(validationMessage);
-
-//     ctx.wizard.state[editField] = messageText;
-//     await deleteKeyboardMarkup(ctx);
-//     // await deleteMessage(ctx, {
-//     //   message_id: (parseInt(ctx.message.message_id) - 1).toString(),
-//     //   chat_id: ctx.message.chat.id,
-//     // });
-//     return ctx.reply(...postingFormatter.editPreview(state), { parse_mode: 'HTML' });
-//   }
-
-//   // if callback exists
-//   // save the mesage id for later deleting
-//   ctx.wizard.state.previousMessageData = {
-//     message_id: ctx.callbackQuery.message.message_id,
-//     chat_id: ctx.callbackQuery.message.chat.id,
-//   };
-//   const callbackMessage = callbackQuery.data;
-
-//   if (callbackMessage == 'post_data') {
-//     // registration
-//     // api call for registration
-//     const response = await QuestionService.createQuestionPost(ctx.wizard.state, callbackQuery.from.id);
-
-//     if (response.success) {
-//       ctx.wizard.state.status = 'pending';
-//       await deleteMessageWithCallback(ctx);
-//       await ctx.reply(...postingFormatter.postingSuccessful());
-//       return ctx.scene.enter('start');
-//     }
-
-//     const registrationAttempt = parseInt(ctx.wizard.state.registrationAttempt);
-
-//     // ctx.reply(...postingFormatter.postingError());
-//     if (registrationAttempt >= 2) {
-//       await deleteMessageWithCallback(ctx);
-//       return ctx.scene.enter('start');
-//     }
-//     return (ctx.wizard.state.registrationAttempt = registrationAttempt ? registrationAttempt + 1 : 1);
-//   } else if (callbackMessage == 'editing_done') {
-//     // await deleteMessageWithCallback(ctx);
-
-//     await ctx.reply(...postingFormatter.preview(state));
-//     return ctx.wizard.back();
-//   }
-
-//   if (fileds.some((filed) => filed == callbackQuery.data)) {
-//     // selecting field to change
-//     ctx.wizard.state.editField = callbackMessage;
-//     await ctx.telegram.deleteMessage(
-//       ctx.wizard.state.previousMessageData.chat_id,
-//       ctx.wizard.state.previousMessageData.message_id,
-//     );
-//     await ctx.reply(...((await postingFormatter.editFieldDispay(callbackMessage)) as any));
-//     if (areEqaul(callbackQuery.data, 'photo', true)) return ctx.wizard.next();
-//     return;
-//   }
-
-//   if (editField) {
-//     //  if edit filed is selected
-
-//     ctx.wizard.state[editField] = callbackMessage;
-//     await deleteMessageWithCallback(ctx);
-//     ctx.wizard.state.editField = null;
-//     return ctx.reply(...postingFormatter.editPreview(state), { parse_mode: 'HTML' });
-//   }
-// }
-// async editPhoto(ctx: any) {
-//   const messageText = ctx.message?.text;
-//   if (messageText && areEqaul(messageText, 'back', true)) {
-//     await deleteMessage(ctx, {
-//       message_id: (parseInt(messageText.message_id) - 1).toString(),
-//       chat_id: messageText.chat.id,
-//     });
-//     ctx.reply(...postingFormatter.editPreview(ctx.wizard.state), { parse_mode: 'HTML' });
-//     return ctx.wizard.back();
-//   }
-
-//   // check if image is attached
-//   if (!ctx.message.photo) return ctx.reply(...postingFormatter.photoPrompt());
-
-//   // Add the image to the array
-//   imagesUploaded.push(ctx.message.photo[0].file_id);
-
-//   // Check if all images received
-//   if (imagesUploaded.length === imagesNumber) {
-//     const file = await ctx.telegram.getFile(ctx.message.photo[0].file_id);
-
-//     const mediaGroup = imagesUploaded.map((image: any) => ({
-//       media: image,
-//       type: 'photo',
-//       caption: image == imagesUploaded[0] ? 'Here are the images you uploaded' : '',
-//     }));
-
-//     await ctx.telegram.sendMediaGroup(ctx.chat.id, mediaGroup);
-
-//     // Save the images to the state
-//     ctx.wizard.state.photo = imagesUploaded;
-
-//     // empty the images array
-//     // imagesUploaded.length = 0;
-//     ctx.reply(...postingFormatter.editPreview(ctx.wizard.state), { parse_mode: 'HTML' });
-//     return ctx.wizard.back();
-//   }
-// }
-
 export default QuestionPostSectionCController;
