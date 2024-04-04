146bd06b997ff483c2d0ad335606d2e2a12062c8
natnael772 <natnaeldeyas0@gmail.com>
2024-04-04 10:52:34 +0300

    feat: Add preview for editing selected fields

--- src/modules/section-3/section3.controller.ts
+++ src/modules/section-3/section3.controller.ts
@@ -168,6 +168,86 @@ class Section3Controller {
       // }
     }
   }
+  async editData(ctx: any) {
+    const state = ctx.wizard.state;
+    const fileds = ['birth_or_marital', 'title', 'description', 'photo', 'cancel'];
+    const callbackQuery = ctx?.callbackQuery;
+    const editField = ctx.wizard.state?.editField;
+    if (!callbackQuery) {
+      // changing field value
+      const messageText = ctx.message.text;
+      if (!editField) return await ctx.reply('invalid input ');
+
+      // const validationMessage = questionPostValidator(ctx.wizard.state.editField, ctx.message.text);
+      // if (validationMessage != 'valid') return await ctx.reply(validationMessage);
+
+      ctx.wizard.state[editField] = messageText;
+      await deleteKeyboardMarkup(ctx);
+      // await deleteMessage(ctx, {
+      //   message_id: (parseInt(ctx.message.message_id) - 1).toString(),
+      //   chat_id: ctx.message.chat.id,
+      // });
+      return ctx.reply(...section3Formatter.editPreview(state), { parse_mode: 'HTML' });
+    }
+
+    // if callback exists
+    // save the mesage id for later deleting
+    ctx.wizard.state.previousMessageData = {
+      message_id: ctx.callbackQuery.message.message_id,
+      chat_id: ctx.callbackQuery.message.chat.id,
+    };
+    const callbackMessage = callbackQuery.data;
+
+    if (callbackMessage == 'post_data') {
+      console.log('Posted Successfully');
+      return ctx.reply(...section3Formatter.postingSuccessful());
+      // registration
+      // api call for registration
+      // const response = await QuestionService.createQuestionPost(ctx.wizard.state, callbackQuery.from.id);
+
+      // if (response.success) {
+      //   ctx.wizard.state.status = 'pending';
+      //   await deleteMessageWithCallback(ctx);
+      //   await ctx.reply(...postingFormatter.postingSuccessful());
+      //   return ctx.scene.enter('start');
+      // }
+
+      const registrationAttempt = parseInt(ctx.wizard.state.registrationAttempt);
+
+      // ctx.reply(...postingFormatter.postingError());
+      if (registrationAttempt >= 2) {
+        await deleteMessageWithCallback(ctx);
+        return ctx.scene.enter('start');
+      }
+      return (ctx.wizard.state.registrationAttempt = registrationAttempt ? registrationAttempt + 1 : 1);
+    } else if (callbackMessage == 'editing_done') {
+      // await deleteMessageWithCallback(ctx);
+
+      await ctx.reply(...section3Formatter.preview(state));
+      return ctx.wizard.back();
+    }
+
+    if (fileds.some((filed) => filed == callbackQuery.data)) {
+      // selecting field to change
+      ctx.wizard.state.editField = callbackMessage;
+      await ctx.telegram.deleteMessage(
+        ctx.wizard.state.previousMessageData.chat_id,
+        ctx.wizard.state.previousMessageData.message_id,
+      );
+      await ctx.reply(...((await section3Formatter.editFieldDisplay(callbackMessage)) as any));
+      if (areEqaul(callbackQuery.data, 'photo', true)) return ctx.wizard.next();
+      return;
+    }
+
+    if (editField) {
+      //  if edit filed is selected
+
+      ctx.wizard.state[editField] = callbackMessage;
+      await deleteMessageWithCallback(ctx);
+      ctx.wizard.state.editField = null;
+      return ctx.reply(...section3Formatter.editPreview(state), { parse_mode: 'HTML' });
+    }
+  }
 }
 
 export default Section3Controller;
