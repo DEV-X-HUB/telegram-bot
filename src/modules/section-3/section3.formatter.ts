364c11c1f43517d42a31a16f8a4feb62feed00e0
natnael772 <natnaeldeyas0@gmail.com>
2024-04-04 10:44:11 +0300

    feat: Add section3 option and prompts

---
+++ src/modules/section-3/section3.formatter.ts
@@ -0,0 +1,31 @@
+import { InlineKeyboardButtons, MarkupButtons } from '../../ui/button';
+import { TableInlineKeyboardButtons, TableMarkupKeyboardButtons } from '../../types/components';
+
+class Section3Formatter {
+  birthOrMaritalOption: TableInlineKeyboardButtons;
+  backOption: TableMarkupKeyboardButtons;
+  constructor() {
+    this.birthOrMaritalOption = [
+      [
+        { text: 'Birth', cbString: 'birth' },
+        { text: 'Marital', cbString: 'marital' },
+      ],
+      [{ text: 'Back', cbString: 'back' }],
+    ];
+    this.backOption = [[{ text: 'Back', cbString: 'back' }]];
+  }
+  birthOrMaritalOptionDisplay() {
+    return ['Choose an option', InlineKeyboardButtons(this.birthOrMaritalOption)];
+  }
+  titlePrompt() {
+    return ['What is the title?', this.goBackButton(false)];
+  }
+  descriptionPrompt() {
+    return ['Enter description maximimum 45 words'];
+  }
+  photoPrompt() {
+    return ['Attach four photos ', this.goBackButton(false)];
+  }
+}
+
+export default Section3Formatter;
