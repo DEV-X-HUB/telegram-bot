57fae9398d2243c73e61fab94ddaea7c673e75e5
natnael772 <natnaeldeyas0@gmail.com>
2024-04-05 13:15:42 +0300

    feat: Add Preview

--- src/modules/question-post/section-4/chicken-farm/chicken-farm.controller.ts
+++ src/modules/question-post/section-4/chicken-farm/chicken-farm.controller.ts
@@ -75,6 +75,64 @@ class ChickenFarmController {
     await ctx.reply(...chickenFarmFormatter.preview(ctx.wizard.state));
     return ctx.wizard.next();
   }
+
+  async preview(ctx: any) {
+    const callbackQuery = ctx.callbackQuery;
+    if (!callbackQuery) {
+      const message = ctx.message.text;
+      if (message == 'Back') {
+        await ctx.reply(...chickenFarmFormatter.descriptionPrompt(), chickenFarmFormatter.goBackButton());
+        return ctx.wizard.back();
+      }
+      await ctx.reply('....');
+    } else {
+      const state = ctx.wizard.state;
+      switch (callbackQuery.data) {
+        case 'preview_edit': {
+          console.log('preview edit');
+          ctx.wizard.state.editField = null;
+          await deleteMessageWithCallback(ctx);
+          ctx.reply(...chickenFarmFormatter.editPreview(state), { parse_mode: 'HTML' });
+          return ctx.wizard.next();
+        }
+
+        case 'editing_done': {
+          // await deleteMessageWithCallback(ctx);
+          await ctx.reply(chickenFarmFormatter.preview(state));
+          return ctx.wizard.back();
+        }
+
+        case 'post_data': {
+          console.log('here you are');
+          // api request to post the data
+          // const response = await QuestionService.createQuestionPost(ctx.wizard.state, callbackQuery.from.id);
+          // console.log(response);
+
+          // if (response?.success) {
+          //   await deleteMessageWithCallback(ctx);
+          await deleteMessageWithCallback(ctx);
+          await displayDialog(ctx, 'Posted successfully');
+          // await ctx.reply(...chickenFarmFormatter.postingSuccessful());
+          await ctx.scene.leave();
+          return ctx.scene.enter('start');
+          // } else {
+          //   ctx.reply(...postingFormatter.postingError());
+          //   if (parseInt(ctx.wizard.state.postingAttempt) >= 2) {
+          //     await deleteMessageWithCallback(ctx);
+          //     return ctx.scene.enter('start');
+          //   }
+
+          // increment the registration attempt
+          // return (ctx.wizard.state.postingAttempt = ctx.wizard.state.postingAttempt
+          //   ? parseInt(ctx.wizard.state.postingAttempt) + 1
+          //   : 1);
+        }
+      }
+      // default: {
+      //   await ctx.reply('DEFAULT');
+      // }
+    }
+  }
 }
 
 export default ChickenFarmController;
