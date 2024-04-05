c0a9efc0758410b4a6c7db23129789fe7d00ca94
natnael772 <natnaeldeyas0@gmail.com>
2024-04-05 13:22:47 +0300

    feat: Add display options/markups

---
+++ src/modules/question-post/section-4/manufacture/manufacture.formatter.ts
@@ -0,0 +1,59 @@
+import { InlineKeyboardButtons, MarkupButtons } from '../../../../ui/button';
+import { TableInlineKeyboardButtons, TableMarkupKeyboardButtons } from '../../../../types/components';
+
+class ManufactureFormatter {
+  estimatedCapitalOption: TableInlineKeyboardButtons;
+  numberOfWorkerOption: TableInlineKeyboardButtons;
+  backOption: TableMarkupKeyboardButtons;
+  messages = {
+    sectorPrompt: 'Specific sector for manufacture?',
+    numberOfWorkerPrompt: 'Number of worker provide?',
+    estimatedCapitalPrompt: 'What is the estimated capital?',
+    enterpriseNamePrompt: 'Name for the small scale enterprise?',
+    descriptionPrompt: 'Enter description maximimum 45 words',
+    photoPrompt: 'Attach four clear images with different angles',
+    postingSuccessful: 'Posted Successfully',
+    displayError: 'Invalid input, please try again',
+    postingError: 'Posting failed',
+  };
+  constructor() {
+    this.estimatedCapitalOption = [
+      [
+        { text: 'Bronze', cbString: 'bronze' },
+        { text: 'Silver', cbString: 'silver' },
+      ],
+      [
+        { text: 'Gold', cbString: 'gold' },
+        { text: 'Platinum', cbString: 'platinum' },
+      ],
+      [
+        { text: 'Diamond', cbString: 'diamond' },
+        { text: 'Back', cbString: 'back' },
+      ],
+    ];
+
+    this.numberOfWorkerOption = [
+      [
+        { text: '1', cbString: '1' },
+        { text: '2', cbString: '2' },
+      ],
+
+      [
+        { text: '3', cbString: '3' },
+        { text: '4', cbString: '4' },
+      ],
+      [
+        { text: '5', cbString: '5' },
+        { text: '6', cbString: '6' },
+      ],
+      [
+        { text: '7', cbString: '7' },
+        { text: 'Back', cbString: 'back' },
+      ],
+    ];
+
+    this.backOption = [[{ text: 'Back', cbString: 'back' }]];
+  }
+}
+
+export default ManufactureFormatter;
