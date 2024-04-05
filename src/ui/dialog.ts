038cb1b7f6e23537dad0bff3aac63dac243be7aa
natnael772 <natnaeldeyas0@gmail.com>
2024-04-05 13:08:35 +0300

    refactor: modify dialog

--- src/ui/dialog.ts
+++ src/ui/dialog.ts
@@ -1,7 +1,7 @@
 import Bot from '../loaders/bot';
 
 // Display notification(popup) when user click on inline keyboard
-async function displayDialog(displayMessage: string, callbackQuery: string, ctx: any) {
+async function displayDialog(ctx: any, displayMessage: string) {
   const bot = Bot();
   if (bot != null) {
     return await ctx.answerCbQuery(displayMessage, { show_alert: true });
