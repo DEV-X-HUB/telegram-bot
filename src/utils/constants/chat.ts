43c3327b067ba70f2fe6e1aa5886721fed0131bb
natnael772 <natnaeldeyas0@gmail.com>
2024-04-26 21:53:56 +0300

    feat: Add channel post function

--- src/utils/constants/chat.ts
+++ src/utils/constants/chat.ts
@@ -1,3 +1,5 @@
+import config from '../../config/config';
+
 export const deleteMessage = async (ctx: any, messageData: { message_id: string; chat_id: string }) => {
   await ctx.telegram.deleteMessage(messageData.chat_id, messageData.message_id);
 };
@@ -40,4 +42,17 @@ export const sendMediaGroup = async (ctx: any, phtos: any[], caption: string = '
     caption: caption,
   }));
   await ctx.telegram.sendMediaGroup(ctx.chat.id, mediaGroup);
+};
+
+export const sendMediaGroupToChannel = async (
+  ctx: any,
+  photos: any[],
+  caption: string = 'Images associated with the post',
+) => {
+  const mediaGroup = photos.map((image: any) => ({
+    media: image,
+    type: 'photo',
+    caption: caption,
+  }));
+  await ctx.telegram.sendMediaGroup(config.channel_id, mediaGroup);
 };
