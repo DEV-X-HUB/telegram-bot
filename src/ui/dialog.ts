c2430be43d78999cfb9e1fdd07ce89e68614d7da
natnael772 <natnaeldeyas0@gmail.com>
2024-06-14 22:40:10 +0300

    refactor: add optional show alert field

--- src/ui/dialog.ts
+++ src/ui/dialog.ts
@@ -1,27 +1,15 @@
 import Bot from '../loaders/bot';
 
 // Display notification(popup) when user click on inline keyboard
-async function displayDialog(ctx: any, displayMessage: string) {
+async function displayDialog(ctx: any, displayMessage: string, show_alert: boolean = false) {
   const bot = Bot();
   if (bot != null) {
     try {
-      return await ctx.answerCbQuery(displayMessage, { show_alert: true });
+      return await ctx.answerCbQuery(displayMessage, { show_alert: show_alert });
     } catch (error) {
       console.log(error);
     }
   }
 }
 
-// Display modal dialog with a message
-async function displayModal(ctx: any, displayMessage: string) {
-  const bot = Bot();
-  if (bot != null) {
-    try {
-      return await ctx.answerCbQuery(displayMessage, true);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
-
-export { displayDialog, displayModal };
+export { displayDialog };
