0532e3d367cceacdacd860b620e41f0fd7bcde30
=Natty-7r <nati7fekadu@gmail.com>
2024-07-22 04:27:44 +0300

    chore: add global error and activity logger

--- src/exception-filters/api-error.filter.ts
+++ src/exception-filters/api-error.filter.ts
@@ -7,32 +7,32 @@ import parseStackTrace from '../utils/helpers/stack-tracer';
 import { v4 as uuid } from 'uuid';
 
 // Global Error Handler
-const APIErrorFilter = (err: any, request: Request, response: Response, next: NextFunction) => {
+const APIErrorFilter = (exception: any, request: Request, response: Response, next: NextFunction) => {
   const errorLogger = new WinstonErrorLogger('API');
   let stackInfo: any;
-  const status = err.status || 'ERROR';
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const status = exception.status || 'ERROR';
+  const statusCode = exception.statusCode || 500;
+  const message = exception.message || 'Internal Server Error';
 
   const loggerResponse = {
     id: uuid(),
-    status: err.statusCode,
+    status: exception.statusCode,
     path: request.url,
     method: request.method,
     ip: request.ip,
     timestamp: new Date().toISOString(),
-    stack: err instanceof Error ? err.stack : '',
+    stack: exception instanceof Error ? exception.stack : '',
   };
 
-  if (err instanceof Error) {
-    stackInfo = parseStackTrace(err?.stack as string);
+  if (exception instanceof Error) {
+    stackInfo = parseStackTrace(exception?.stack as string);
   }
 
   errorLogger.log(typeof message !== 'string' ? (message as any).message : message, {
     ...stackInfo,
     ...loggerResponse,
   });
-  response.status(err.getStatus()).json({
+  response.status(exception.getStatus()).json({
     statusCode,
     message,
     status,
