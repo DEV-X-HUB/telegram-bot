0a88b975b47df5092ad95f9094812fa78f429583
Natty-7r <nati7fekadu@gmail.com>
2025-01-17 00:39:49 +0300

    chore: clear dist folder

--- dist/utils/generatePassword.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function generateOTP(digits = 5) {
-    // Generate a random otp of specified length
-    const otp = Math.random().toString().slice(-digits);
-    return otp;
-}
-exports.default = generateOTP;
-//# sourceMappingURL=generatePassword.js.map
