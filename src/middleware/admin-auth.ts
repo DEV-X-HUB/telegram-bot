7ececc0dda284dfe61c09ddd497d9d8fddb3c79e
Natty-7/r <nati7fekadu@gmail.com>
2025-07-13 12:04:01 +0300

    fix: update auth guard

--- src/middleware/admin-auth.ts
+++ src/middleware/admin-auth.ts
@@ -3,9 +3,10 @@ import jwt from 'jsonwebtoken';
 import { JwtAuthPayload, RequestWithUser } from '../types/api';
 import config from '../config/config';
 
+import prisma from '../loaders/db-connecion';
 // Middleware to extract and verify token
 
-export const authGuard = (req: RequestWithUser, res: Response, next: NextFunction) => {
+export const authGuard = async (req: RequestWithUser, res: Response, next: NextFunction) => {
   try {
     const authHeader = req.headers.authorization;
     const escapedRoutes = ['/auth/forgot', '/auth/login', '/auth/reset', '/auth/verify'];
@@ -19,6 +20,10 @@ export const authGuard = (req: RequestWithUser, res: Response, next: NextFunctio
     const token = authHeader.split(' ')[1];
 
     const decoded = jwt.verify(token, config.jwt.secret as string) as JwtAuthPayload;
+
+    const userInDb = await prisma.admin.findFirst({ where: { id: decoded.id } });
+    if (!userInDb) return res.status(401).json({ message: 'Unauthorized' });
+
     req.user = decoded; // Attach user info to request object
     next();
   } catch (err) {
